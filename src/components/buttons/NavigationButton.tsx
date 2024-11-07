c97450bc165415328c5bdad6e8987dc54b3ddd31
Andres Arango <andresap@proton.me>
2024-11-07 11:22:23 -0500

    Update NavigationButton

--- src/components/buttons/NavigationButton.tsx
+++ src/components/buttons/NavigationButton.tsx
@@ -1,17 +1,27 @@
 "use client";
 
+import { useIsPathActive } from "@/hooks/navigation";
 import { Box, useTheme } from "@mui/material";
 import Button from "@mui/material/Button";
 import NextLink from "next/link";
+import { usePathname } from "next/navigation";
 import { cloneElement, ReactElement } from "react";
 
 interface NavigationButtonProps {
   icon?: ReactElement | null;
   text: string;
   href: string;
-  selected: boolean;
+  selected?: boolean;
 }
 
+/**
+ * A functional component that renders a circular SVG indicator.
+ * The circle is filled with a secondary color from the theme if `isVisible` is true.
+ *
+ * @param {Object} props - The component props.
+ * @param {boolean} props.isVisible - Determines if the circle should be filled with the theme's secondary color.
+ * @returns {JSX.Element} The rendered SVG element.
+ */
 function SelectedIndicator({ isVisible }: { isVisible: boolean }) {
   const theme = useTheme();
   return (
@@ -28,13 +38,32 @@ function SelectedIndicator({ isVisible }: { isVisible: boolean }) {
   );
 }
 
+/**
+ * A navigation button component that renders a button with an icon and text.
+ * If the `selected` attribute is not provided, the button can be selected based on the current pathname.
+ *
+ * @param {Object} props - The properties object.
+ * @param {ReactElement} props.icon - The icon to be displayed in the button.
+ * @param {string} props.text - The text to be displayed in the button.
+ * @param {string} props.href - The href link for the button.
+ * @param {boolean} [props.selected] - Optional. Indicates if the button is selected.
+ *
+ * @returns {JSX.Element} The rendered navigation button component.
+ */
 export default function NavigationButton({
   icon,
   text,
   href,
   selected,
 }: NavigationButtonProps) {
-  const iconColor = selected ? "secondary" : "inherit";
+  const pathname = usePathname();
+  const isActive = useIsPathActive(pathname);
+
+  if (selected === undefined) {
+    selected = isActive(href);
+  }
+
+  const iconColor = selected ? "action.selected" : "inherit";
   return (
     <Button
       href={href}
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { useIsPathActive } from "@/hooks/navigation";
-import { Box, Button, Container, Toolbar, Typography } from "@mui/material";
+import { Box, Container, Toolbar, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import NextLink from "next/link";
-import { usePathname } from "next/navigation";
 import NavigationButton from "../buttons/NavigationButton";
 import ThemeToggleSwitch from "../buttons/ThemeToggleSwitch";
 
+/**
+ * NavBar component renders the top navigation bar of the application.
+ * It includes the application title, navigation buttons, and a theme toggle switch.
+ *
+ * @returns {JSX.Element} The rendered NavBar component.
+ */
 export default function NavBar() {
-  const pathname = usePathname();
-
-  const isActive = useIsPathActive(pathname);
-
   return (
     <AppBar position="static">
       <Container>
@@ -30,46 +29,10 @@ export default function NavBar() {
             }}
             gap={2}
           >
-            <NavigationButton href="/" text="Home" selected={isActive("/")} />
-            <Button
-              href="/finances"
-              component={NextLink}
-              passHref
-              color="inherit"
-              sx={{
-                backgroundColor: isActive("/finances")
-                  ? "action.selected"
-                  : "transparent",
-              }}
-            >
-              Finances
-            </Button>
-            <Button
-              href="/trips"
-              component={NextLink}
-              passHref
-              color="inherit"
-              sx={{
-                backgroundColor: isActive("/trips")
-                  ? "action.selected"
-                  : "transparent",
-              }}
-            >
-              Trips
-            </Button>
-            <Button
-              href="/vehicles"
-              component={NextLink}
-              passHref
-              color="inherit"
-              sx={{
-                backgroundColor: isActive("/vehicles")
-                  ? "action.selected"
-                  : "transparent",
-              }}
-            >
-              Vehicles
-            </Button>
+            <NavigationButton href="/" text="Home" />
+            <NavigationButton href="/finances" text="Finances" />
+            <NavigationButton href="/trips" text="Trips" />
+            <NavigationButton href="/vehicles" text="Vehicles" />
           </Box>
           <ThemeToggleSwitch />
         </Toolbar>
